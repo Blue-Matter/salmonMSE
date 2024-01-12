3f8dfcab1eee0ea51d891209f4751145cc3ee3f3
Adrian Hordyk <ar.hordyk@gmail.com>
2024-01-12 09:30:27 -0800

    new R package

---
+++ R/Enhancement.R
@@ -0,0 +1,17 @@
+
+
+#' Hatchery Enhancement Evaluator
+#'
+#' An example function.
+#'
+#' @param Pars A list of key parameters
+#' @param ... Other arguments
+#'
+#' @return A list of answers
+#' @export
+#'
+#' @examples
+#' myresults <- Enhancement()
+Enhancement <- function(Pars=NULL, ...) {
+
+}
