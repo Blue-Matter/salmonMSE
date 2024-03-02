7090bfca9051bc13416797def83afd069c07d2a3
Quang Huynh <quang@bluematterscience.com>
2024-03-01 16:55:18 -0800

    Rename fitness_env

--- R/fitness_env.R
+++
@@ -1,6 +0,0 @@
-
-
-#' Environment to store fitness calculates during the simulation
-#'
-#' @export
-fitness_env <- new.env()
---
+++ R/salmonMSE_env.R
@@ -0,0 +1,6 @@
+
+
+#' Environment to store salmon specific variables, e.g, fitness, during the simulation
+#'
+#' @export
+salmonMSE_env <- new.env()
