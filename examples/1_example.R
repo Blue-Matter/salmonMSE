b001e6be2c12b82b23f8bd6332ae1be3998debd1
Quang Huynh <quang@bluematterscience.com>
2024-03-14 16:27:27 -0700

    Add example

---
+++ examples/1_example.R
@@ -0,0 +1,65 @@
+
+
+# Install packages
+remotes::install_github("Blue-Matter/MSEtool", ref = "salmonMSE")
+remotes::install_github("Blue-Matter/salmonMSE")
+
+
+library(salmonMSE)
+
+Bio <- new(
+  "Bio",
+  nsim = 3,
+  maxage = 3,
+  p_mature = c(0, 0, 1),
+  capacity_smolt = 17250,   # Beverton-Holt asymptote. Not unfished capacity!!
+  prod_smolt = 300,         # Productivity adult/SAR. At unfished equilibrium the realized smolt productivity = 1/SAR
+  SAR = 0.01,               # Future feature to allow for time-varying (PDO forcing)
+  fec = 5040,               # Spawning fecundity of NOS and HOS
+  p_female = 0.49
+  #strays = 0
+)
+
+Hatchery <- new(
+  "Hatchery",
+  n_yearling = 0,                   # Management lever. No hatchery if both this line and next line are zero
+  n_subyearling = 2e6,              # Management lever. No hatchery if both this line and previous line are zero
+  #n_subyearling = 0,
+  s_prespawn = 1,                   # Survival prior to spawning
+  s_egg_smolt = 1e-6,               # Survival of eggs in hatchery
+  s_egg_subyearling = 0.92,
+  gamma = 0.8,
+  pmax_NOB = 0.7,
+  ptarget_NOB = 0.51,
+  premove_HOS = 0,
+  theta = c(100, 80, 70),
+  rel_loss = c(0.5, 0.4, 0.1),
+  fec_brood = 5040,
+  fitness_type = "Ford",
+  pbar_start = c(93.1, 92),
+  fitness_variance = 10,
+  selection_strength = 3,
+  heritability = 0.5,
+  fitness_floor = 0.5
+)
+
+Habitat <- new(
+  "Habitat",
+  capacity_smolt_improve = 1,    # Keep carrying capacity (SR alpha/beta) constant
+  prod_smolt_improve = 1         # Keep productivity (SR alpha) constant
+)
+
+Harvest <- new(
+  "Harvest",
+  u = c(0.038, 0.025, 0, 0.14) %>% sum(),   # Specify fixed harvest rate of mature fish
+  m = 0                                     # Mark rate of hatchery releases (future feature)
+)
+
+# Stitched salmon operating model
+SOM <- new("SOM", Bio, Habitat, Hatchery, Harvest)
+
+# run salmonMSE
+SMSE <- MMSE2SMSE(MMSE, SOM, N = salmonMSE_env$N)
+
+# run AHA
+SAHA <- AHA(SOM)
