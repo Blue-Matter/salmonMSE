88c221c38c7b7f908b9d944940f2c31c6b16f72d
Quang Huynh <quang@bluematterscience.com>
2024-09-20 18:17:58 -0700

    Replace to WILD50

--- R/pm.R
+++ R/pm.R
@@ -4,7 +4,7 @@
 #' Functions that evaluate return probabilities of outcomes from the simulations.
 #'
 #' - `PNI75` calculates the probability that PNI exceeds 0.75
-#' - `WILD80` calculates the probability that at least 80 percent of natural spawners are wild
+#' - `WILD50` calculates the probability that at least 50 percent of natural spawners are wild
 #'
 #' @param SMSE SMSE object returned by [salmonMSE()]
 #' @param Ref Threshold for the performance metric, used to calculate the probability that the metric exceeds this value
@@ -19,7 +19,7 @@ PNI75 <- function(SMSE, Ref = 0.75, Yrs = NULL) {
 
 #' @rdname PNI75
 #' @export
-WILD80 <- function(SMSE, Ref = 0.80, Yrs = NULL) {
+WILD50 <- function(SMSE, Ref = 0.50, Yrs = NULL) {
   if (is.null(Yrs)) Yrs <- c(1, SMSE@proyears)
   apply(SMSE@p_wild[,,Yrs[1]:Yrs[2]] >= Ref, 2, mean, na.rm = TRUE)
 }
