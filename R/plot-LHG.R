2672a2d10a026339757c8c7fa3585735e2f85d10
Quang Huynh <quang@bluematterscience.com>
2025-02-07 16:08:36 -0800

    Plot absolute abundance by LHG

--- R/plot-LHG.R
+++ R/plot-LHG.R
@@ -5,6 +5,7 @@
 #'
 #' @param SMSE Class \linkS4class{SMSE} object returned by [salmonMSE()]
 #' @param var Character. Slot for the state variables in `SMSE@Misc$LHG[[1]]`.
+#' @param type Character to indicate whether to plot proportion or absolute numbers
 #' @param s Integer. Population index for multi-population model (e.g., `s = 1` is the first population in the model)
 #' @param FUN Summarizing function across simulations, typically [median()] or [mean()]
 #' @param xlab Character. Name of time variable for the figure
@@ -14,9 +15,11 @@
 #' @importFrom grDevices hcl.colors
 #' @seealso [plot_statevar_ts()]
 #' @export
-plot_LHG <- function(SMSE, var = "NOS", s = 1, FUN = median, figure = TRUE, xlab = "Projection Year",
-                     ylab = paste("Proportion", var), LHG_names) {
+#' @importFrom graphics box
+plot_LHG <- function(SMSE, var = "NOS", type = c("prop", "abs"), s = 1, FUN = median, figure = TRUE, xlab = "Projection Year",
+                     ylab, LHG_names) {
 
+  type <- match.arg(type)
   LHG <- SMSE@Misc$LHG[[s]]
 
   if (length(LHG)) {
@@ -30,7 +33,11 @@ plot_LHG <- function(SMSE, var = "NOS", s = 1, FUN = median, figure = TRUE, xlab
     x <- apply(x, c(1, 2, 4), sum)
   }
 
-  xplot <- apply(x, c(1, 3), function(i) i/sum(i)) %>% apply(c(1, 3), FUN)
+  if (type == "abs") {
+    xplot <- apply(x, 2:3, FUN)
+  } else {
+    xplot <- apply(x, c(1, 3), function(i) i/sum(i)) %>% apply(c(1, 3), FUN)
+  }
 
   if (figure && any(xplot > 0, na.rm = TRUE)) {
     ind <- colSums(xplot, na.rm = TRUE) > 0
@@ -39,11 +46,20 @@ plot_LHG <- function(SMSE, var = "NOS", s = 1, FUN = median, figure = TRUE, xlab
     if (missing(LHG_names)) LHG_names <- paste("LHG", 1:nrow(xplot))
 
     col <- grDevices::hcl.colors(nrow(xplot), palette = "Pastel 1")
+
+    if (type == "abs") {
+      ylim <- c(0, 1.1) * range(colSums(xplot, na.rm = TRUE))
+      if (missing(ylab)) ylab <- var
+    } else {
+      ylim <- c(0, 1)
+      if (missing(ylab)) ylab <- paste("Proportion", var)
+    }
     plot(Year, NULL, xlim = range(Year) + c(-1, 0),
          xlab = "Projection Year", ylab = ylab,
-         typ = "n", ylim = c(0, 1), xaxs = "i", yaxs = "i")
+         typ = "n", ylim = ylim, xaxs = "i", yaxs = "i")
     barplot(xplot, legend.text = LHG_names, space = 0, xlim = range(Year),
             col = col, border = "grey40", add = TRUE, xpd = FALSE)
+    box()
   }
 
   invisible(xplot)
