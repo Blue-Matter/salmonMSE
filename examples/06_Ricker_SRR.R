03cea9f3141a01a0e60dd22b7750e45a37648a16
Quang Huynh <quang@bluematterscience.com>
2025-07-21 13:15:01 -0700

    Update Ricker SRR comparison (convert Smax to units of eggs)

--- examples/06_Ricker_SRR.R
+++ examples/06_Ricker_SRR.R
@@ -8,8 +8,8 @@
 source("https://raw.githubusercontent.com/Pacific-salmon-assess/samEst/refs/heads/main/R/RP_functions.R")
 
 
-compare_ref <- function(alpha = 3,
-                        Smax = 1000,
+compare_ref <- function(alpha = 3, # Units of recruits/spawner
+                        Smax = 1000, # Units of spawners
                         maxage = 5,
                         rel_F = c(0, 1),
                         p_female = 1,
@@ -20,6 +20,7 @@ compare_ref <- function(alpha = 3,
                         s_enroute = 1,
                         p_mature = c(0, 0.1, 0.2, 0.3, 1)) {
 
+  # Calculate eggs per smolt
   phi <- salmonMSE:::calc_phi(
     Mjuv = Mjuv,
     p_mature = p_mature,
@@ -30,23 +31,43 @@ compare_ref <- function(alpha = 3,
     p_LHG = 1
   )
 
+  # To convert Smax from units of spawners to eggs:
+  # 1. Calculate unfished spawners (from Ricker SRR, set R = S --> S0 = exp(1/Smax)/alpha)
+  # 2. Calculate spawners per smolt (spro)
+  # 3. Calculate egg per smolt (phi)
+  # 4. unfished eggs (eo) = unfished spawners * smolt per spawner * egg per smolt
+  # 5. Smax_eggs = log(alpha) / unfished eggs
+  spro <- salmonMSE:::calc_phi(
+    Mjuv = Mjuv,
+    p_mature = p_mature,
+    p_female = p_female,
+    fec = matrix(fec, maxage, 1),
+    s_enroute = s_enroute,
+    n_g = 1,
+    p_LHG = 1,
+    output = "spawner"
+  )
+
+  so <- Smax * log(alpha)
+  eo <- so / spro * phi
+  beta_eggs <- log(alpha)/eo
+  Smax_eggs <- 1/beta_eggs
+
   SRRpars <- data.frame(
     kappa = alpha,
-    Smax = Smax,
+    Smax = Smax_eggs,
     phi = phi,
     SRrel = "Ricker"
   )
 
   ref <- salmonMSE:::calc_MSY(
-    matrix(Mjuv, maxage, 1), fec, p_female, rel_F, vulPT, vulT, matrix(p_mature, maxage, 1),
-    s_enroute, n_g = 1, p_LHG = 1,
-    SRRpars
+    Mjuv, fec, p_female, rel_F, vulPT, vulT, p_mature,
+    s_enroute, SRRpars = SRRpars
   )
 
   ref["Sgen"] <- salmonMSE:::calc_Sgen(
-    matrix(Mjuv, maxage, 1), fec, p_female, rel_F, vulPT, vulT, matrix(p_mature, maxage, 1),
-    s_enroute, n_g = 1, p_LHG = 1,
-    SRRpars, SMSY = ref["Spawners_MSY"]
+    Mjuv, fec, p_female, rel_F, vulPT, vulT, p_mature,
+    s_enroute, SRRpars = SRRpars, SMSY = ref["Spawners_MSY"]
   )
 
   ref_salmonMSE <- ref[c("UT_MSY", "Spawners_MSY", "Sgen")] |> structure(names = c("UMSY", "SMSY", "Sgen"))
