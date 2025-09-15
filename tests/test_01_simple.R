d195e036d23b29bcb77e9f5a3acff6fe916fd9b6
Quang Huynh <quang@bluematterscience.com>
2025-09-15 12:31:22 -0700

    Update MSF test

--- tests/test_01_simple.R
+++ tests/test_01_simple.R
@@ -13,8 +13,18 @@ test_that("Simple SMSE with MSF", {
 
   expect_s4_class(SMSE_MSF, "SMSE")
   expect_lt(max(SMSE_MSF@KT_NOS), 1e-8)  # No natural origin retained catch
-  expect_true(round(max(SMSE_MSF@ExT_HOS), 3) == u_terminal)  # HO ex. rate = Harvest rate
+  expect_true(round(max(SMSE_MSF@ExT_HOS), 3) == u_terminal)  # HO ex. rate = Harvest rate because m = 1
+
+  m <- SMSE_MSF@Misc$SOM@Hatchery[[1]]@m
+  delta <- SMSE_MSF@Misc$SOM@Harvest[[1]]@release_mort[2]
+
+  F_kept <- -log(1 - max(SMSE_MSF@ExT_HOS))
+  E <- F_kept/m
+  F_NO <- delta * E
+  Ex_NO <- 1 - exp(-F_NO)
+
   expect_gt(max(SMSE_MSF@ExT_NOS), 0)  # NO ex. rate > 0
+  expect_equal(round(max(SMSE_MSF@ExT_NOS), 4), round(Ex_NO, 4))
 })
 
 
