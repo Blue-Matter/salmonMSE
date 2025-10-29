5b3c59af8ab00e6c4efdd3b9c9671b5478dd7246
Quang Huynh <quang@bluematterscience.com>
2025-10-29 13:19:19 -0700

    Add CM2SOM warning

--- R/CM.R
+++ R/CM.R
@@ -197,8 +197,11 @@ sample_CM <- function(fit, ...) {
 
 #' Convert conditioning model to operating model
 #'
+#' @description
 #' Creates an operating model from MCMC samples and data inputs of the conditioning model.
 #' Management actions for habitat, hatchery production, and harvest still need to be specified in the operating model.
+#'
+#' **Note: the function assumes the maturity values in the last conditioning year for the projection, which are likely not well informed by CWT data. Consider updating the maturity using some historical average (e.g., across most recent completed brood years).**
 #'
 #' @param stanfit Output from [`sample_CM()`]
 #' @param sims Optional, a vector of integers indicating the MCMC iterations to convert to operating model simulations. Otherwise,
