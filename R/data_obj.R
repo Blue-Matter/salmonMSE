04105d579d0feb707295136d000f26999ae98d37
Quang Huynh <quang@bluematterscience.com>
2026-03-11 22:07:10 -0700

    Add data object

---
+++ R/data_obj.R
@@ -0,0 +1,15 @@
+
+
+#' Example objects from salmonMSE
+#'
+#' S4 objects containing operating model inputs to run the projection.
+#'
+#' @format An object of class [salmonMSE::SOM-class].
+#' @source See salmonMSE [website](https://docs.salmonmse.com/articles/example.html)
+#'
+#' @aliases multi_SOM
+#' @examples
+#' data(simple_SOM)
+#'
+#' data(multi_SOM)
+"simple_SOM"
